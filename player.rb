742fd127fc8fbad5fbfc13ed1a61d274593e6214
Andre Menezes <djdedemenezes@gmail.com>
2023-10-28 22:56:08 -0300

    add player basic class

---
+++ player.rb
@@ -0,0 +1,14 @@
+class Player
+  def initialize(window)
+    @image = Gosu::Image.new('images/ship.png')
+
+    @x = 200
+    @y = 200
+    @angle = 0
+  end
+
+  def draw
+    # draw_rot(x, y, z = 0, angle = 0, center_x = 0.5, center_y = 0.5, scale_x = 1, scale_y = 1, color = 0xff_ffffff, mode = :default)
+    @image.draw_rot(@x, @y, 0, @angle)
+  end
+end
