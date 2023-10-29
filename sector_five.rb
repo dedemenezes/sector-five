de6b955ad4a7a404de56edb48e29feba1b417ae7
Andre Menezes <djdedemenezes@gmail.com>
2023-10-29 01:53:14 -0300

    add explosions to game

--- sector_five.rb
+++ sector_five.rb
@@ -4,6 +4,7 @@ require 'gosu'
 require_relative 'player'
 require_relative 'enemy'
 require_relative 'bullet'
+require_relative 'explosion'
 
 # Runs the game
 class SectorFive < Gosu::Window
@@ -19,40 +20,57 @@ class SectorFive < Gosu::Window
     @player = Player.new(self)
     @enemies = []
     @bullets = []
+    @explosions = []
   end
 
   def update
-    @player.turn_left if button_down?(Gosu::KB_LEFT)
-    @player.turn_right if button_down?(Gosu::KB_RIGHT)
-    @player.accelerate if button_down?(Gosu::KB_UP)
-    @player.move
-
-    @enemies << Enemy.new(self) if rand < ENEMY_FREQUENCY
-    @enemies.each(&:move)
-    @bullets.each(&:move)
-
-    @enemies.dup.each do |enemy|
-      @bullets.dup.each do |bullet|
-        distance = Gosu.distance(enemy.x, enemy.y, bullet.x, bullet.y)
-        if distance < enemy.radius + enemy.radius
-          @enemies.delete(enemy)
-          @bullets.delete(bullet)
-        end
-      end
-    end
-
-
+    update_player
+    update_enemies
+    update_bullets
+    war_casualties
   end
 
   def draw
     @player.draw
     @enemies.each(&:draw)
     @bullets.each(&:draw)
+    @explosions.each(&:draw)
   end
 
   def button_down(id)
-    if id == Gosu::KbSpace
-      @bullets.push Bullet.new(self, @player.x, @player.y, @player.angle)
+    return unless id == Gosu::KbSpace
+
+    @bullets.push Bullet.new(self, @player.x, @player.y, @player.angle)
+  end
+
+  private
+
+  def update_player
+    @player.turn_left if button_down?(Gosu::KB_LEFT)
+    @player.turn_right if button_down?(Gosu::KB_RIGHT)
+    @player.accelerate if button_down?(Gosu::KB_UP)
+    @player.move
+  end
+
+  def update_enemies
+    @enemies << Enemy.new(self) if rand < ENEMY_FREQUENCY
+    @enemies.each(&:move)
+  end
+
+  def update_bullets
+    @bullets.each(&:move)
+  end
+
+  def war_casualties
+    @enemies.dup.each do |enemy|
+      @bullets.dup.each do |bullet|
+        distance = Gosu.distance(enemy.x, enemy.y, bullet.x, bullet.y)
+        if distance < enemy.radius + enemy.radius
+          @enemies.delete(enemy)
+          @bullets.delete(bullet)
+          @explosions << Explosion.new(self, enemy.x,enemy.y)
+        end
+      end
     end
   end
 end
