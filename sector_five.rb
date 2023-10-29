cffd98747824fd232b4434859ac2c030bdd8f014
Andre Menezes <djdedemenezes@gmail.com>
2023-10-29 00:03:03 -0300

    add basioc enemy class moving down

--- sector_five.rb
+++ sector_five.rb
@@ -2,6 +2,7 @@
 
 require 'gosu'
 require_relative 'player'
+require_relative 'enemy'
 
 # Runs the game
 class SectorFive < Gosu::Window
@@ -13,6 +14,8 @@ class SectorFive < Gosu::Window
     self.caption = 'Sector Five'
 
     @player = Player.new(self)
+    @enemy = Enemy.new(self)
+    @enemies = []
   end
 
   def update
@@ -20,10 +23,12 @@ class SectorFive < Gosu::Window
     @player.turn_right if button_down?(Gosu::KB_RIGHT)
     @player.accelerate if button_down?(Gosu::KB_UP)
     @player.move
+    @enemy.move
   end
 
   def draw
     @player.draw
+    @enemy.draw
   end
 end
 
