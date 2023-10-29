df5be1f4ddb25d21cbe1179335baf70dd201bd47
Andre Menezes <djdedemenezes@gmail.com>
2023-10-29 01:44:38 -0300

    add explosion class

---
+++ explosion.rb
@@ -0,0 +1,20 @@
+class Explosion
+  def initialize(window, x, y)
+    @window = window
+    @x = x
+    @y = y
+    @images = Gosu::Image.load_tiles('images/explosions.png', 60, 60)
+    @radius = 30
+    @finished = false
+    @image_index = 0
+  end
+
+  def draw
+    if @image_index < @images.count
+      @images[@image_index].draw(@x - @radius, @y - @radius, 2)
+      @image_index += 1
+    else
+      @finished = true
+    end
+  end
+end
