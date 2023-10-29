e699d5370863742868f6adcd93b10bfe9091dd98
Andre Menezes <djdedemenezes@gmail.com>
2023-10-29 02:45:23 -0300

    refac box module

--- bullet.rb
+++ bullet.rb
@@ -6,7 +6,7 @@ require_relative 'box'
 class Bullet
   include Box
   SPEED = 5
-  attr_reader :x, :y, :radius
+  attr_reader :x, :y, :radius, :window
 
   def initialize(window, x, y, angle)
     @image = Gosu::Image.new('images/bullet.png')
